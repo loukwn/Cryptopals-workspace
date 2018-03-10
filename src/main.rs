e4322dcf386eae6c2fb598fab5b2b896cf0f961a
Kostantinos Lountzis <loukwn@gmail.com>
2018-03-10 21:37:17 +0200

    Second challenge complete

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
-mod partA;
+mod part_a;
 
 fn main() {
-    let testInput = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d".to_string();
-    let base64String = partA::hex_to_base64(testInput);
+	// 1
+    let test_input = String::from("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
+    let base64_string = part_a::hex_to_base64(test_input);
 
-    println!("Result: {}", base64String);
+    println!("Result: {}", base64_string);
+
+    // 2
+    let a = String::from("1c0111001f010100061a024b53535009181c");
+    let b = String::from("686974207468652062756c6c277320657965");
+
+    let xored_string = part_a::fixed_xor(a,b);
+
+    println!("Result2: {}", xored_string);
 }
---
+++ src/part_a/mod.rs
@@ -0,0 +1,35 @@
+extern crate base64;
+extern crate hex;
+
+use std::u8;
+
+pub fn hex_to_base64(hex: String) -> String {
+
+    let mut bytes = Vec::new();
+    for i in 0..(hex.len()/2) {
+        let res = u8::from_str_radix(&hex[2*i .. 2*i+2], 16);
+        match res {
+            Ok(v) => bytes.push(v),
+            Err(e) => println!("Problem with hex: {}", e),
+        };
+    };
+
+    base64::encode(&bytes)
+}
+
+pub fn fixed_xor(str_a: String, str_b: String) -> String {
+
+    let buffer_a = match hex::decode(str_a) {
+		Ok(bytes) => bytes,
+		Err(_) => panic!("Could not decode"),
+    };
+
+    let buffer_b = match hex::decode(str_b) {
+		Ok(bytes) => bytes,
+		Err(_) => panic!("Could not decode"),
+    };
+
+	let key_iter = buffer_b.iter().cycle();
+	let res : Vec<u8> = buffer_a.iter().zip(key_iter).map(|(&a, b)| a ^ b ).collect();
+    hex::encode(res)
+}
